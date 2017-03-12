9f9e69319fa6bc58f9b2fafcf1ef5b1a0e8843cf
a24230928 <a24230928@gmail.com>
2017-03-12 17:58:25 +0800

    this is my first extention

---
+++ content.js
@@ -0,0 +1,3 @@
+var file = '<div class="button" style="cursor: pointer;padding-right:0;" onclick="$(\'input[type=file]\').trigger(\'click\');"><i style="padding:0 6px;" class="material-icons"></i></div>';
+$('#textBox').css('width','90%');
+$('div#sendButton').before(file);
