74461e35c6f7ef0363a03b217ab4ca5f887efc99
a24230928 <a24230928@gmail.com>
2017-03-14 22:24:25 +0800

    No longer rely jqeury

--- content.js
+++ content.js
@@ -1,3 +1,7 @@
-var file = '<div class="button" style="cursor: pointer;padding-right:0;" onclick="$(\'input[type=file]\').trigger(\'click\');"><i style="padding:0 6px;" class="material-icons"></i></div>';
-$('#textBox').css('width','90%');
-$('div#sendButton').before(file);
+var inner = '<div><div><div id="changeButton"><input value="離開" onclick="changePerson()" type="button"></div>';
+inner += '<div id="textBox"><input type="text" id="messageInput" placeholder="輸入訊息" autocomplete="off"></div>';
+inner += '<div class="button" style="cursor: pointer;padding-right:0;" onclick="$(\'input[type=file]\').trigger(\'click\');">';
+inner += '<i style="padding:0 6px;" class="material-icons"></i></div><div id="sendButton">';
+inner += '<input type="button" value="傳送" onclick="sendMessage(); return false;"></div></div></div>';
+document.getElementById('textBox').style.width = "90%";
+document.getElementById('sendBox').innerHTML = inner;
